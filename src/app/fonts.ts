50ba0b3dfb0a95ab89e96c30daf205d87f546ecf
Guilherme Figueira <guibf2004@gmail.com>
2023-07-09 13:31:39 -0300

    initial commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins, Open_Sans } from "next/font/google";
+
+export const poppins = Poppins({ subsets: ["latin"], weight: ["400", "600"] });
+
+export const open_sans = Open_Sans({
+	subsets: ["latin"],
+	weight: ["400"],
+});
